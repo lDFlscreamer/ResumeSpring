fd9621ce5add0494149b2e4b1b74c197998619af
Screamer <999screamer999@gmail.com>
2022-07-16 17:02:42 +0300

    receive get request

--- src/main/java/com/screamer/resume/controller/AuthController.java
+++ src/main/java/com/screamer/resume/controller/AuthController.java
@@ -1,23 +1,22 @@
 package com.screamer.resume.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping(path = "api/public", produces = MediaType.APPLICATION_JSON_VALUE)
 public class AuthController {
-    @RequestMapping(
-            value = "/user",
-            method = RequestMethod.POST)
+    @PostMapping(value ="/user" )
     public void process(@RequestBody Map<String, Object> payload)
             throws Exception {
 
         System.out.println(payload);
+    }
+    @GetMapping(value ="/user" )
+    public String test() {
+        return "test.entrySet()";
     }
 }
