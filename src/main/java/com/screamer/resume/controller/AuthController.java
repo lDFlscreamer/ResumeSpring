19f656f8c05156c17d4b21b81b94332adf33aa34
Screamer <999screamer999@gmail.com>
2022-07-16 17:22:51 +0300

    testing auth0 method

--- src/main/java/com/screamer/resume/controller/AuthController.java
+++ src/main/java/com/screamer/resume/controller/AuthController.java
@@ -1,6 +1,8 @@
 package com.screamer.resume.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
@@ -9,12 +11,16 @@ import java.util.Map;
 
 @RestController
 public class AuthController {
+    Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     @PostMapping(value ="/user" )
     public void process(@RequestBody Map<String, Object> payload)
             throws Exception {
-
-        System.out.println(payload);
+        logger.info(payload.toString());
     }
+
+
+
     @GetMapping(value ="/user" )
     public String test() {
         return "test.entrySet()";
