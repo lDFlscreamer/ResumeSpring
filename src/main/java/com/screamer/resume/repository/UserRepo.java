0de14c9de7274630df00ab58647ea64aa03a3a7f
Captain Screamer <999screamer999@gmail.com>
2022-07-28 19:28:33 +0300

    Delete UserRepo.java
    this cant exist with object like type  so need to be tempolary remove

--- src/main/java/com/screamer/resume/repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.screamer.resume.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepo extends MongoRepository<Object,String> {
-}
