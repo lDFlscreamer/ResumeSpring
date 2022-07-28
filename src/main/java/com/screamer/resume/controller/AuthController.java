613087c8767adaa9d97270bfe8353a7891875852
Screamer <999screamer999@gmail.com>
2022-07-28 19:02:06 +0300

    prepare to merge into master

--- src/main/java/com/screamer/resume/controller/AuthController.java
+++ src/main/java/com/screamer/resume/controller/AuthController.java
@@ -10,26 +10,49 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * This Controller for auth0 purpose
+ *
+ * Auth0 logIn ,logOut, change user profile and get user endpoints
+ */
+// TODO: 28.07.2022 change realisation of auth0 methods
 @RestController
 public class AuthController {
     Logger logger = LoggerFactory.getLogger(AuthController.class);
 
+    /**
+     * @param payload auth0 user profile
+     * @return  registered user
+     * @throws Exception DB exception all another
+     */
+    // TODO: 28.07.2022 change auth0 signIn to true behavior and interaction with DB
     @PostMapping(
             value = "/user",
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, Object> process(@RequestBody Map<String, Object> payload)
+    public Map<String, Object> signIn(@RequestBody Map<String, Object> payload)
             throws Exception {
         logger.info(payload.toString());
         return payload;
     }
 
+
+    /**
+     * @param headers  parameter of user ( like email and pass)
+     * @return depends on auth0 behavior
+     */
+    // TODO: 28.07.2022 change auth0 logIn to true behavior and interaction with DB
     @GetMapping(value = "/user")
     @ResponseStatus(HttpStatus.CREATED)
-    public String test(@RequestBody Map<String, String> headers) {
+    public String logIn(@RequestBody Map<String, String> headers) {
         logger.info(headers.toString());
         return null;
     }
 
+    /**
+     *
+     * @return user profile from DB
+     */
+    // TODO: 28.07.2022 change auth0 GetUser to true behavior and interaction with DB
     @GetMapping(value = "/GetUser",
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Map<String, String> getUser() {
--- src/main/java/com/screamer/resume/repository/UserRepo.java
+++ src/main/java/com/screamer/resume/repository/UserRepo.java
@@ -1,9 +1,8 @@
 package com.screamer.resume.repository;
 
-import com.screamer.resume.entity.ResumeUser;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepo extends MongoRepository<ResumeUser,String> {
+public interface UserRepo extends MongoRepository<Object,String> {
 }
