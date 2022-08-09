431227a66d2f3995ac397d50504d361e441f13d2
Screamer <999screamer999@gmail.com>
2022-08-09 12:42:49 +0300

    [feature]  configure HttpSecurity by creating a SecurityFilterChain bean instead of deprecated way.

--- src/main/java/com/screamer/resume/config/SecurityConfig.java
+++ src/main/java/com/screamer/resume/config/SecurityConfig.java
@@ -5,15 +5,15 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig  {
 
     @Value("${auth0.audience}")
     private String audience;
@@ -21,8 +21,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
     private String issuer;
 
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .mvcMatchers("/").permitAll()
                 .mvcMatchers("/api").authenticated()
@@ -30,6 +30,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().cors()
                 .and().oauth2ResourceServer().jwt();
         http.csrf().disable();
+        return http.build();
     }
 
     @Bean
