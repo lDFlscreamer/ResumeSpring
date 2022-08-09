9180fcfaf3dfd00ca54960e922f093d6bcdf8697
Screamer <999screamer999@gmail.com>
2022-08-09 21:39:54 +0300

    [fix] remove unused import

--- src/main/java/com/screamer/resume/ResumeApplication.java
+++ src/main/java/com/screamer/resume/ResumeApplication.java
@@ -1,19 +1,14 @@
 package com.screamer.resume;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ResumeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ResumeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeApplication.class, args);
+    }
 
 
 }
