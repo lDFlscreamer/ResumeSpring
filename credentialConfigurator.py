8953768b3f2d768f48e33336fe84eedc90ff2849
Screamer <999screamer999@gmail.com>
2022-08-09 16:21:34 +0300

    [fix] declare and divide for two methods.

--- credentialConfigurator.py
+++ credentialConfigurator.py
@@ -1,14 +1,26 @@
 import os
 
-f = open("./src/main/resources/application.properties", "a")
-f.write("#Creadetials")
-f.write(f"auth0.audience={os.getenv('AUTH0_AUDIENCE')}")
-f.write(f"auth0.domain={os.getenv('AUTH0_DOMAIN')}")
-f.write(f"")
-f.write(f"linkedin.consumerKey={os.getenv('LINKED_IN_CONSUMER_KEY')}")
-f.write(f"linkedin.consumerSecret={os.getenv('LINKED_IN_CONSUMER_SECRET')}")
-f.write(f"")
-f.write(f"spring.data.mongodb.database={os.getenv('MONGO_DB_NAME')}")
-f.write(f"spring.data.mongodb.password={os.getenv('MONGO_DB_PASS')}")
-f.write(f"spring.data.mongodb.username={os.getenv('MONGO_DB_USERNAME')}")
-f.close()
+
+def write_to_properties(propertiesFileSrc: str):
+    f = open(propertiesFileSrc, "a")
+    f.write("#Creadetials")
+    f.write(f"auth0.audience={os.getenv('AUTH0_AUDIENCE')}")
+    f.write(f"auth0.domain={os.getenv('AUTH0_DOMAIN')}")
+    f.write(f"")
+    f.write(f"linkedin.consumerKey={os.getenv('LINKED_IN_CONSUMER_KEY')}")
+    f.write(f"linkedin.consumerSecret={os.getenv('LINKED_IN_CONSUMER_SECRET')}")
+    f.write(f"")
+    f.write(f"spring.data.mongodb.database={os.getenv('MONGO_DB_NAME')}")
+    f.write(f"spring.data.mongodb.password={os.getenv('MONGO_DB_PASS')}")
+    f.write(f"spring.data.mongodb.username={os.getenv('MONGO_DB_USERNAME')}")
+    f.close()
+
+def read_from_properties(propertiesFileSrc: str):
+    f = open(propertiesFileSrc, "r")
+    print(f.read())
+
+if __name__ == "__main__":
+    propertiesFileSrc = "./src/main/resources/application.properties"
+    write_to_properties(propertiesFileSrc)
+    read_from_properties(propertiesFileSrc)
+
