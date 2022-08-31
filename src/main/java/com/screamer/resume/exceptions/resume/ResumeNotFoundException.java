28594a2a01c61b694640f09e89eac16d11e7ef63
Screamer <999screamer999@gmail.com>
2022-08-31 18:08:45 +0300

    [feature] added resumeId field into Exception

--- src/main/java/com/screamer/resume/exceptions/resume/ResumeNotFoundException.java
+++ src/main/java/com/screamer/resume/exceptions/resume/ResumeNotFoundException.java
@@ -1,7 +1,14 @@
 package com.screamer.resume.exceptions.resume;
 
 public class ResumeNotFoundException extends Exception {
+    private final String resumeId;
+
     public ResumeNotFoundException(String id) {
         super(String.format("Resume with id %s doesnt exist", id));
+        this.resumeId = id;
+    }
+
+    public String getResumeId() {
+        return resumeId;
     }
 }
