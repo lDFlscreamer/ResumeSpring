4c31736a22b55c254b861b9e02608c2cbaf5126f
Screamer <999screamer999@gmail.com>
2022-08-31 17:56:57 +0300

    [feature] add messageId field into exception

--- src/main/java/com/screamer/resume/exceptions/message/MessageNotFoundException.java
+++ src/main/java/com/screamer/resume/exceptions/message/MessageNotFoundException.java
@@ -1,7 +1,15 @@
 package com.screamer.resume.exceptions.message;
 
 public class MessageNotFoundException extends Exception {
+
+    private final String messageId;
+
     public MessageNotFoundException(String id) {
         super(String.format("Messages with id %s doesnt exist", id));
+        this.messageId = id;
+    }
+
+    public String getMessageId() {
+        return messageId;
     }
 }
--- src/main/java/com/screamer/resume/exceptions/message/MessageUnansweredException.java
+++ src/main/java/com/screamer/resume/exceptions/message/MessageUnansweredException.java
@@ -1,7 +1,15 @@
 package com.screamer.resume.exceptions.message;
 
 public class MessageUnansweredException extends Exception {
+
+    private final String messageId;
+
     public MessageUnansweredException(String id) {
         super(String.format("Messages with id %s doesnt have answer", id));
+        this.messageId = id;
+    }
+
+    public String getMessageId() {
+        return messageId;
     }
 }
