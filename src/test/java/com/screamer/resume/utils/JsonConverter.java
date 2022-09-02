25646d60a0c3324b91c682b5efe9b60e177ab179
Screamer <999screamer999@gmail.com>
2022-09-02 20:14:15 +0300

    [feature] add convertJsonToListOfObject

--- src/test/java/com/screamer/resume/utils/JsonConverter.java
+++ src/test/java/com/screamer/resume/utils/JsonConverter.java
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import org.springframework.boot.test.context.TestComponent;
 import org.springframework.http.MediaType;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @TestComponent
 public class JsonConverter {
@@ -29,9 +31,15 @@ public class JsonConverter {
         return ow.writeValueAsString(object);
     }
 
-    public  <T> T convertJsonToMap(String json, Class<T> objectClass) throws IOException {
+    public  <T> T convertJsonToObject(String json, Class<T> objectClass) throws IOException {
         ObjectMapper mapper = new ObjectMapper();
         return mapper.reader().readValue(json, objectClass);
+    }
+
+    public  <T> List<T> convertJsonToListOfObject(String json, Class<T> objectClass) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        CollectionType collectionType = mapper.getTypeFactory().constructCollectionType(List.class, objectClass);
+        return mapper.readValue(json, collectionType);
     }
 
 }
