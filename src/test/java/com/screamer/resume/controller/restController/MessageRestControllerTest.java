5d9e1a6ee7208ddc5c6c769e2a4dbf3d3338a74d
Screamer <999screamer999@gmail.com>
2022-09-02 21:01:20 +0300

    [feature] add MessageRestController Tests

--- src/test/java/com/screamer/resume/controller/restController/MessageRestControllerTest.java
+++ src/test/java/com/screamer/resume/controller/restController/MessageRestControllerTest.java
@@ -1,24 +1,25 @@
 package com.screamer.resume.controller.restController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.ObjectWriter;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.screamer.resume.config.JwtDecoderTestConfig;
 import com.screamer.resume.entity.Message;
-import com.screamer.resume.service.dbServices.message.MessageDbService;
-import org.junit.jupiter.api.DisplayName;
+import com.screamer.resume.service.businessServices.message.MessageService;
+import com.screamer.resume.utils.JsonConverter;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -26,103 +27,211 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@Import({JwtDecoderTestConfig.class, JsonConverter.class})
 @AutoConfigureMockMvc
 class MessageRestControllerTest {
 
-    private final MediaType APPLICATION_JSON_UTF8 = new MediaType(
-            MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            StandardCharsets.UTF_8);
+    @Autowired
+    private JsonConverter jsonConverter;
+
     @MockBean
-    private MessageDbService messageDbService;
+    private MessageService messageService;
     @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @DisplayName("Get /message Test")
     void getAllMessage() throws Exception {
-        when(messageDbService.getAllSavedMessages()).thenReturn(new ArrayList<>());
+        List<Message> messageList = new ArrayList<>();
+        messageList.add(new Message());
+        messageList.add(new Message());
 
-        this.mockMvc
-                .perform(get("/message"))
+        when(messageService.getAllMessage()).thenReturn(messageList);
+
+        MvcResult result = this.mockMvc
+                .perform(
+                        get("/message")
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer token"))
+                .andExpect(status().isOk())
                 .andDo(print())
-                .andExpect(status().isUnauthorized());
+                .andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        List<Message> messages = jsonConverter.convertJsonToListOfObject(contentAsString, Message.class);
+
+        assertEquals(messageList.size(), messages.size(), "message list do not match");
     }
 
     @Test
-    @DisplayName("Get /message/public Test")
+    void getAllMessage_withoutJwt() throws Exception {
+        this.mockMvc
+                .perform(
+                        get("/message"))
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
+    }
+
+    @Test
     void getPublicMessage() throws Exception {
-        when(messageDbService.getAllSavedMessages()).thenReturn(new ArrayList<>());
+        List<Message> messageList = new ArrayList<>();
+        messageList.add(new Message());
+        messageList.add(new Message());
 
-        this.mockMvc
-                .perform(get("/message/public"))
+        when(messageService.getPublicMessage()).thenReturn(messageList);
+
+        MvcResult result = this.mockMvc
+                .perform(
+                        get("/message/public")
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer token"))
+                .andExpect(status().isOk())
                 .andDo(print())
-                .andExpect(status().isOk());
+                .andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        List<Message> messages = jsonConverter.convertJsonToListOfObject(contentAsString, Message.class);
+
+        assertEquals(messageList.size(), messages.size(), "message list do not match");
     }
 
     @Test
-    @DisplayName("post /message tests")
+    void getPublicMessage_withoutJwt() throws Exception {
+        List<Message> messageList = new ArrayList<>();
+        messageList.add(new Message());
+        messageList.add(new Message());
+
+        when(messageService.getPublicMessage()).thenReturn(messageList);
+
+        MvcResult result = this.mockMvc
+                .perform(
+                        get("/message/public"))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        List<Message> messages = jsonConverter.convertJsonToListOfObject(contentAsString, Message.class);
+
+        assertEquals(messageList.size(), messages.size(), "message list do not match");
+    }
+
+
+    @Test
     void createNewMessage() throws Exception {
-        Message message = createTestMessage();
-        when(messageDbService.saveNewMessage(any(Message.class))).thenReturn(message);
-        String requestJson = convertMessageDtoToJSON(message);
+        Message mockMessage = new Message();
+        String mockMessageJson = jsonConverter.convertObjectToJSON(mockMessage);
 
-        this.mockMvc
-                .perform(post("/message")
-                        .contentType(APPLICATION_JSON_UTF8)
-                        .content(requestJson))
+        when(messageService.createNewMessage(any(Authentication.class), any(Message.class)))
+                .thenAnswer(m -> m.getArguments()[1]);
+
+        MvcResult result = this.mockMvc
+                .perform(
+                        post("/message")
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer token")
+                                .contentType(jsonConverter.getContentType())
+                                .content(mockMessageJson))
+                .andExpect(status().isCreated())
                 .andDo(print())
-                .andExpect(status().isCreated());
-    }
+                .andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        Message message = jsonConverter.convertJsonToObject(contentAsString, Message.class);
 
-    private Message createTestMessage() {
-        Message message = new Message();
-        message.setPublicAuthorName("authorTest");
-        message.setTitle("titleTest");
-        message.setContent("contentTest");
-        return message;
-    }
-
-    private String convertMessageDtoToJSON(Message message) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.configure(SerializationFeature.WRAP_ROOT_VALUE, false);
-        ObjectWriter ow = mapper.writer().withDefaultPrettyPrinter();
-        return ow.writeValueAsString(message);
+        assertEquals(mockMessage.get_id(), message.get_id(), "message id do not match");
     }
 
     @Test
-    @DisplayName("PUT /message/{id} test")
+    void createNewMessage_withoutJwt() throws Exception {
+        Message mockMessage = new Message();
+        String mockMessageJson = jsonConverter.convertObjectToJSON(mockMessage);
+
+        when(messageService.createNewMessage(any(), any(Message.class)))
+                .thenAnswer(m -> m.getArguments()[1]);
+
+        MvcResult result = this.mockMvc
+                .perform(
+                        post("/message")
+                                .contentType(jsonConverter.getContentType())
+                                .content(mockMessageJson))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        Message message = jsonConverter.convertJsonToObject(contentAsString, Message.class);
+
+        assertEquals(mockMessage.get_id(), message.get_id(), "message id do not match");
+    }
+
+    @Test
     void updateMessage() throws Exception {
-        Message message = createTestMessage();
-        when(messageDbService.updateMessage(any(Message.class))).thenReturn(message);
-        String requestJson = convertMessageDtoToJSON(message);
+        Message mockMessage = new Message();
+        String mockMessageJson = jsonConverter.convertObjectToJSON(mockMessage);
+
+        when(messageService.updateMessage(any(Message.class)))
+                .thenAnswer(m -> m.getArguments()[0]);
+
+        MvcResult result = this.mockMvc
+                .perform(
+                        put("/message")
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer token")
+                                .contentType(jsonConverter.getContentType())
+                                .content(mockMessageJson))
+                .andExpect(status().isCreated())
+                .andDo(print())
+                .andReturn();
+        String contentAsString = result.getResponse().getContentAsString();
+        Message message = jsonConverter.convertJsonToObject(contentAsString, Message.class);
+
+        assertEquals(mockMessage.get_id(), message.get_id(), "message id do not match");
+    }
+
+    @Test
+    void updateMessage_withoutJwt() throws Exception {
+        Message mockMessage = new Message();
+        String mockMessageJson = jsonConverter.convertObjectToJSON(mockMessage);
 
         this.mockMvc
-                .perform(put("/message")
-                        .contentType(APPLICATION_JSON_UTF8)
-                        .content(requestJson))
-                .andExpect(status().isUnauthorized());
+                .perform(
+                        put("/message")
+                                .contentType(jsonConverter.getContentType())
+                                .content(mockMessageJson))
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
     }
 
     @Test
-    @DisplayName("DELETE /message/{id} test")
     void deleteMessageById() throws Exception {
         String messageId = UUID.randomUUID().toString();
 
         this.mockMvc
-                .perform(delete("/message/".concat(messageId)))
-                .andDo(print())
-                .andExpect(status().isUnauthorized());
+                .perform(
+                        delete("/message/".concat(messageId))
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer token"))
+                .andExpect(status().isNoContent())
+                .andDo(print());
     }
 
     @Test
-    @DisplayName("Delete all message test")
-    void deleteAllMessages() throws Exception {
+    void deleteMessageById_withoutJwt() throws Exception {
+        String messageId = UUID.randomUUID().toString();
 
         this.mockMvc
-                .perform(delete("/message"))
-                .andDo(print())
-                .andExpect(status().isUnauthorized());
+                .perform(
+                        delete("/message/".concat(messageId)))
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
     }
 
+    @Test
+    void deleteAllMessages() throws Exception {
+        this.mockMvc
+                .perform(
+                        delete("/message")
+                                .header(HttpHeaders.AUTHORIZATION, "Bearer token"))
+                .andExpect(status().isNoContent())
+                .andDo(print());
+    }
+
+    @Test
+    void deleteAllMessages_withoutJwt() throws Exception {
+        this.mockMvc
+                .perform(
+                        delete("/message"))
+                .andExpect(status().isUnauthorized())
+                .andDo(print());
+    }
 }
