0cefc8fcc4eae12ffa16ea8b796e0dfe50c00ee5
Screamer <999screamer999@gmail.com>
2022-09-05 18:36:51 +0300

    [feature] create JwtAuthenticationConverter bean to correct retrieve User role from Auth0 jwt

--- src/main/java/com/screamer/resume/config/SecurityConfig.java
+++ src/main/java/com/screamer/resume/config/SecurityConfig.java
@@ -9,18 +9,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
 import org.springframework.security.oauth2.core.OAuth2TokenValidator;
 import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableWebSecurity
 public class SecurityConfig  {
-
-    @Value("${auth0.audience}")
-    private String audience;
-
-    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
-    private String issuer;
 
     @Bean
     public DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
@@ -42,19 +38,6 @@ public class SecurityConfig  {
                     .oauth2ResourceServer().jwt();
         http.csrf().disable();
         return http.build();
-    }
-
-    @Bean
-    JwtDecoder jwtDecoder() {
-        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
-
-        OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
-        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
-        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
-
-        jwtDecoder.setJwtValidator(withAudience);
-
-        return jwtDecoder;
     }
 
     @Bean
